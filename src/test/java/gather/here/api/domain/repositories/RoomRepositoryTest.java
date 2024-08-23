3d23dc17a105e2c7700836353586c0b6773d5b2d
kimsanhaa <ttksgk@gmail.com>
2024-08-23 12:28:17 +0900

    Feat: embedded redis 의존성 추가

--- src/test/java/gather/here/api/domain/repositories/RoomRepositoryTest.java
+++ src/test/java/gather/here/api/domain/repositories/RoomRepositoryTest.java
@@ -1,44 +1,43 @@
-//package gather.here.api.domain.repositories;
-//
-//import gather.here.api.domain.entities.LocationShareEvent;
-//import org.assertj.core.api.Assertions;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.ActiveProfiles;
-//
-//import java.util.UUID;
-//
-//@SpringBootTest
-//@ActiveProfiles("test")
-//class RoomRepositoryTest {
-//
-//    @Autowired
-//    private RoomRepository roomRepository;
-//
-//    @Test
-//    public void saveTest(){
-//        Long roomSeq = 1L;
-//        Long memberSEq = 1L;
-//        String sessionId = String.valueOf(UUID.randomUUID());
-//        String nickname = "RoomRepositoryTest";
-//        String imageUrl = "https://imageUrl.imageurl.com";
-//        Float presentLat = 1F;
-//        Float presentLng = 1F;
-//        Float destinationDistance = 1F;
-//        LocationShareEvent locationShareEvent = LocationShareEvent.create(
-//                roomSeq,
-//                memberSEq,
-//                sessionId,
-//                nickname,
-//                imageUrl,
-//                presentLat,
-//                presentLng,
-//                destinationDistance
-//        );
-//        roomRepository.saveLocationShareEvent(locationShareEvent);
-//        LocationShareEvent locationShareEventByRoomSeq = roomRepository.findLocationShareEventByRoomSeq(1L);
-//        Assertions.assertThat(locationShareEventByRoomSeq).isNotNull();
-//    }
-//
-//}
+package gather.here.api.domain.repositories;
+
+import gather.here.api.domain.entities.LocationShareEvent;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.UUID;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class RoomRepositoryTest {
+
+    @Autowired
+    private RoomRepository roomRepository;
+
+    @Test
+    public void saveTest(){
+        Long roomSeq = 2L;
+        Long memberSEq = 1L;
+        String sessionId = String.valueOf(UUID.randomUUID());
+        String nickname = "RoomRepositoryTest";
+        String imageUrl = "https://imageUrl.imageurl.com";
+        Float presentLat = 1F;
+        Float presentLng = 1F;
+        Float destinationDistance = 1F;
+        LocationShareEvent locationShareEvent = LocationShareEvent.create(
+                roomSeq,
+                memberSEq,
+                sessionId,
+                nickname,
+                imageUrl,
+                presentLat,
+                presentLng,
+                destinationDistance
+        );
+        roomRepository.saveLocationShareEvent(locationShareEvent);
+        LocationShareEvent locationShareEventByRoomSeq = roomRepository.findLocationShareEventByRoomSeq(1L);
+        Assertions.assertThat(locationShareEventByRoomSeq).isNotNull();
+    }
+}
