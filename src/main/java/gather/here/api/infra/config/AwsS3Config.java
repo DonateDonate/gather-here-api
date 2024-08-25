9ba7e09e749da1c2e67d2f7e3c4d978ef8010fe5
kimsanhaa <ttksgk@gmail.com>
2024-08-25 12:58:18 +0900

    Fix: aws config secretkey, secretKey 수정

--- src/main/java/gather/here/api/infra/config/AwsS3Config.java
+++ src/main/java/gather/here/api/infra/config/AwsS3Config.java
@@ -6,19 +6,22 @@ import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Slf4j
 @Configuration
 public class AwsS3Config {
+    @Value("${aws.s3.access.key}")
+    private String accessKey;
+
+    @Value("${aws.s3.secret.key}")
+    private String secretKey;
 
     @Bean
     public AmazonS3Client amazonS3Client() {
-        String sanha = "";
-        String sd = "";
-
-        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(sanha, sd);
+        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
         return (AmazonS3Client) AmazonS3ClientBuilder.standard()
                 .withRegion(Regions.AP_NORTHEAST_2).enablePathStyleAccess()
                 .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
