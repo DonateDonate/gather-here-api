e2dc3e04d191b0141c1008b5dfbdb9292bc536cb
kimsanhaa <ttksgk@gmail.com>
2024-09-12 12:53:56 +0900

    Fix: redis password 추가

--- src/main/java/gather/here/api/infra/config/RedisConfig.java
+++ src/main/java/gather/here/api/infra/config/RedisConfig.java
@@ -1,9 +1,11 @@
 package gather.here.api.infra.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
@@ -11,17 +13,23 @@ import org.springframework.data.redis.repository.configuration.EnableRedisReposi
 @Configuration
 @EnableRedisRepositories
 public class RedisConfig {
+    private static final Logger log = LoggerFactory.getLogger(RedisConfig.class);
+
     @Value("${redis.host}")
     private String host;
 
     @Value("${redis.port}")
     private int port;
 
-    @Bean
-    public RedisConnectionFactory redisConnectionFactory(){
-        return new LettuceConnectionFactory(host,port);
-    }
+    @Value("${redis.password}")
+    private String password;
 
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(host,port);
+        config.setPassword(password);
+        return new LettuceConnectionFactory(config);
+    }
     @Bean
     public RedisTemplate<?, ?> redisTemplate() {
         RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
