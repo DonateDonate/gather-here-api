13de9416c255e40d03fd20d60639e88b8585e957
kimsanhaa <ttksgk@gmail.com>
2024-09-30 15:17:14 +0900

    Fix: text, binary buffer size 수정/ timeout 수정

--- src/main/java/gather/here/api/infra/config/WebSocketConfig.java
+++ src/main/java/gather/here/api/infra/config/WebSocketConfig.java
@@ -9,6 +9,7 @@ import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 @Configuration
 @EnableWebSocket
@@ -27,4 +28,12 @@ public class WebSocketConfig implements WebSocketConfigurer {
         return new CustomWebSocketHandler(webSocketService);
     }
 
+    @Bean
+    public ServletServerContainerFactoryBean createWebSocketContainer(){
+        ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+        container.setMaxBinaryMessageBufferSize(8192);
+        container.setMaxTextMessageBufferSize(8192);
+        container.setMaxSessionIdleTimeout(600000L);
+        return container;
+    }
 }
