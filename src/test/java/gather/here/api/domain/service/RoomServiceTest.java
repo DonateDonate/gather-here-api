93dd2f0df3a6c315660bb17ad896e5f8c3290cc9
kimsanhaa <ttksgk@gmail.com>
2024-10-15 10:57:25 +0900

    Feat: websocketService log 생성

--- src/test/java/gather/here/api/domain/service/RoomServiceTest.java
+++ src/test/java/gather/here/api/domain/service/RoomServiceTest.java
@@ -1,7 +1,6 @@
 package gather.here.api.domain.service;
 
 import gather.here.api.domain.repositories.MemberRepository;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +11,6 @@ import org.springframework.test.context.web.WebAppConfiguration;
 @SpringBootTest
 @ActiveProfiles("test")
 @WebAppConfiguration
-@Disabled
 class RoomServiceTest {
 
     @Autowired
@@ -25,6 +23,7 @@ class RoomServiceTest {
     @DisplayName("sut는 존재하는 room이 있으면 예외가 발생한다")
     public void createRoomAlreadyExistTest(){
         //arrange
+
 
 
         //act
